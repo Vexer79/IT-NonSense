6874d882b122654283977667b0c2a42e9f551e1d
Vexer79 <rokytskyi.i@gmail.com>
2024-05-30 01:34:59 +0200

    feat: add a little bit funny content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./ad.css";
+import Ad from "./components/Ad";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <Ad></Ad>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Ad.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Ad() {
+    return (
+        <div className="ad">
+            <img src="../image/funny.png" alt="Our best friend" />
+        </div>
+    );
+}
