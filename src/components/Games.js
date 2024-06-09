ba4c57899dee29c83bc83e45585a2a223c6f8a6b
Vexer79 <rokytskyi.i@gmail.com>
2024-06-09 15:00:51 +0200

    feat: add api

--- src/components/Games.js
+++ src/components/Games.js
@@ -4,44 +4,15 @@ import GameCell from "./GameCell";
 import "../css/Games.css";
 
 export default function Games() {
-    const [games, setGames] = useState([
-        {
-            id: "1",
-            title: "Test Game g fd gnfdgndgf n gfn gfd ngf",
-            pageUrl: "game/test",
-            imageUrl: "image/games/game2.png",
-        },
-        {
-            id: "2",
-            title: "Test Game",
-            pageUrl: "game/test",
-            imageUrl: "image/games/game2.png",
-        },
-        {
-            id: "3",
-            title: "Test Game",
-            pageUrl: "game/test",
-            imageUrl: "image/games/game2.png",
-        },
-        {
-            id: "4",
-            title: "Test Game",
-            pageUrl: "game/test",
-            imageUrl: "image/games/game2.png",
-        },
-        {
-            id: "5",
-            title: "Test Game",
-            pageUrl: "game/test",
-            imageUrl: "image/games/game2.png",
-        },
-        {
-            id: "6",
-            title: "Test Game",
-            pageUrl: "game/test",
-            imageUrl: "image/games/game2.png",
-        },
-    ]);
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch("https://it-nonsenseapi.onrender.com/games")
+            .then((response) => response.json())
+            .then((games) => {
+                setGames(games);
+            });
+    });
 
     return (
         <div className="container">
